39fa26675640c98dc02280f21b5505b9c7bd7567
Leo Hanisch <23164374+HaaLeo@users.noreply.github.com>
2020-01-22 00:08:59 +0100

    Make visualization more dynamic

--- swarmlib/pso/visualizer.py
+++ swarmlib/pso/visualizer.py
@@ -21,8 +21,10 @@ class Visualizer:
         self.__iteration_number = kwargs['iteration_number']
         self.__interval = kwargs['interval']
         self.__continuous = kwargs['continuous']
+
         self.__positions = []
         self.__velocities = []
+        self.__frame_interval = 50  # ms
 
         x = np.linspace(self.__lower_boundary, self.__upper_boundary, 100)
         y = np.linspace(self.__lower_boundary, self.__upper_boundary, 100)
@@ -49,12 +51,15 @@ class Visualizer:
 
     def add_data(self, particle_positions: Iterable[Tuple[float, float]], particle_velocities: Iterable[Tuple[float, float]]) -> None:
         x_pos, y_pos = zip(*particle_positions)
-        self.__positions.append([x_pos, y_pos])
+        self.__positions.append([np.array(x_pos), np.array(y_pos)])
 
         vel_x, vel_y = zip(*particle_velocities)
-        self.__velocities.append([vel_x, vel_y])
+        self.__velocities.append([np.array(vel_x), np.array(vel_y)])
 
     def replay(self):
+        # Amount of frames to play considering the number of iterations to show and the __frame_interval update rate
+        frames = int((self.__iteration_number+1)*self.__interval/self.__frame_interval)
+
         def __init():
             self.__particles.set_data([], [])
 
@@ -71,23 +76,34 @@ class Visualizer:
         def __animate(i):
             marker_size = int(50 * self.__fig.get_figwidth()/self.__fig.dpi)
             self.__rectangle.set_edgecolor('k')
-            self.__fig.canvas.set_window_title(
-                'Iteration %s/%s' % (i, self.__iteration_number))
+            ax = self.__fig.gca(label='myAxes')
+
+            # Get the index of the current data to show
+            index = int(np.floor(i / (frames/(self.__iteration_number+1))))
+            self.__fig.canvas.set_window_title(f'Iteration {index}/{self.__iteration_number}')
+
+            # Calculate the scale to apply to the data in order to generate a more dynamic visualization
+            scale = (i-index*(frames/(self.__iteration_number+1))) / (frames/(self.__iteration_number+1))
+
+            # Calculate scaled position and velocity
+            x_data, y_data = self.__positions[index]
+            vel_x, vel_y = self.__velocities[index+1]
+            pos_x_scaled = np.clip(x_data + scale * vel_x, a_min=self.__lower_boundary, a_max=self.__upper_boundary)
+            pos_y_scaled = np.clip(y_data + scale * vel_y, a_min=self.__lower_boundary, a_max=self.__upper_boundary)
+            vel_x_scaled = (1-scale)*vel_x
+            vel_y_scaled = (1-scale)*vel_y
 
             # Update the particle position
-            x_data, y_data = self.__positions[i]
-            self.__particles.set_data(x_data, y_data)
+            self.__particles.set_data(pos_x_scaled, pos_y_scaled)
             self.__particles.set_markersize(marker_size)
 
             # Update the velocities
-            ax = self.__fig.gca(label='myAxes')
-            vel_x, vel_y = self.__velocities[i+1]
-            self.__particle_vel = ax.quiver(x_data, y_data, vel_x, vel_y, angles='xy', scale_units='xy', scale=1, color='#CFCFCF', width=marker_size*0.001)
+            self.__particle_vel = ax.quiver(pos_x_scaled, pos_y_scaled, vel_x_scaled, vel_y_scaled, angles='xy', scale_units='xy', scale=1, color='#CFCFCF', width=marker_size*0.001)
 
             return self.__particles, self.__rectangle, self.__particle_vel
 
-            # iteration_number+1 for initialization frame
-        _ = animation.FuncAnimation(self.__fig, __animate, frames=self.__iteration_number+1, interval=self.__interval,
+        # iteration_number+1 for initialization frame
+        _ = animation.FuncAnimation(self.__fig, __animate, frames=frames, interval=self.__frame_interval,
                                     blit=True, init_func=__init, repeat=self.__continuous)
 
         plt.show()
