8339507dbcb6352de2ba1f27d7e399626e662f70
Leo Hanisch <23164374+HaaLeo@users.noreply.github.com>
2020-02-16 23:37:22 +0100

    Add tsp file to distribution

--- setup.py
+++ setup.py
@@ -7,18 +7,20 @@ from os import path
 
 from setuptools import find_packages, setup
 
-#pylint: disable=exec-used,undefined-variable
+# pylint: disable=exec-used,undefined-variable
 
-with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), 'r') as rf:
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), 'r', encoding='utf8') as rf:
     LONG_DESCRIPTION = rf.read()
 
-with open(path.join(path.abspath(path.dirname(__file__)), 'swarmlib/_version.py'), 'r') as f:
+with open(path.join(path.abspath(path.dirname(__file__)), 'swarmlib/_version.py'), 'r', encoding='utf8') as f:
     exec(f.read())
 
 setup(
     name='swarmlib',  # PEP8: Packages should also have short, all-lowercase names, the use of underscores is discouraged
     version=__version__,
     packages=find_packages(exclude=['*test']),
+    # Include files specified in MANIFEST.in
+    include_package_data=True,
     description='Implementation and visualization of different swarm optimization algorithms.',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
