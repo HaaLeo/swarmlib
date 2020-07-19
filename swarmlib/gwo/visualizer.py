3af1407491eaa0136b694974f4fd90eaa50e9375
Nimish Verma <nimishverma@ymail.com>
2020-07-19 13:51:09 -0400

    removed trailing newline

--- swarmlib/gwo/visualizer.py
+++ swarmlib/gwo/visualizer.py
@@ -11,6 +11,4 @@ class Visualizer(BaseVisualizer):
         self._velocities = [self._positions[index+1]-position for index, position in enumerate(self._positions[:-1])]
         self._velocities.insert(0, np.zeros(self._positions[0].shape))
         self._velocities.append(np.zeros(self._positions[0].shape))
-
         super().replay()
-
