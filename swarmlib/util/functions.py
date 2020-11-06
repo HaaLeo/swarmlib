eab4bcbb732365bd2865a84880606405188366d6
Alexander Fimpel <alxfmpl@gmail.com>
2020-11-06 22:34:27 +0100

    Remove swarmlib's included objective functions

--- swarmlib/util/functions.py
+++ swarmlib/util/functions.py
@@ -6,29 +6,9 @@
 #pylint: disable=invalid-name
 
 import inspect
-from functools import reduce
 
 import landscapes.single_objective
 import numpy as np
-
-
-def michalewicz(x):
-    m = 10
-
-    def func(x):
-        return np.sin(x) * np.power(np.sin((0 + 1) * np.power(x, 2) / np.pi), 2 * m)
-
-    result = reduce(lambda acc, x: acc + func(x), x, 0.0)
-    return -result
-
-
-def ackley(x, a=20, b=0.2, c=2*np.pi):
-
-    x = np.asarray_chkfinite(x)  # ValueError if any NaN or Inf
-    n = len(x)
-    s1 = np.sum(x**2, axis=0)
-    s2 = np.sum(np.cos(c * x), axis=0)
-    return -a*np.exp(-b*np.sqrt(s1 / n)) - np.exp(s2 / n) + a + np.exp(1)
 
 
 # Wrapper for landscapes.single_objective functions for inputs > 1d
@@ -46,5 +26,3 @@ FUNCTIONS = {
     )
     if name not in ['colville', 'wolfe'] # Don't include 3D and 4D functions
 }
-# Replace / add functions that are defined in this file
-FUNCTIONS.update({'michalewicz': michalewicz, 'ackley': ackley})
